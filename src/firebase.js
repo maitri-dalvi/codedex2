d2c7cba51eb4c27025f25d438b8ba78f1f41c5b4
maitri-dalvi <maitridalvi2004@gmail.com>
2024-07-11 02:15:38 +0530

    update

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API,
+  apiKey: "process.env.FIREBASE_API",
   authDomain: "codedex-9472d.firebaseapp.com",
   projectId: "codedex-9472d",
   storageBucket: "codedex-9472d.appspot.com",
